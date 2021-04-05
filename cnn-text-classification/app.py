335ae4c62b12c049171575a31127f9807f5f10d1
Dang nguyen <dang.nguyen@upskills.com>
2021-04-05 18:23:03 +0700

    modify app.py

--- cnn-text-classification/app.py
+++ cnn-text-classification/app.py
@@ -19,3 +19,27 @@ print(y_train.shape)
 print('Shape of test data: ')
 print(X_test.shape)
 print(y_test.shape)
+
+# Padding the data samples to a maximum review length in words
+max_words = 100
+X_train = sequence.pad_sequences(X_train, maxlen=max_words)
+X_test = sequence.pad_sequences(X_test, maxlen=max_words)
+# Building the CNN Model
+model = Sequential()      # initilaizing the Sequential nature for CNN model
+# Adding the embedding layer which will take in maximum of 450 words as input and provide a 32 dimensional output of those words which belong in the top_words dictionary
+model.add(Embedding(top_words, 32, input_length=max_words))
+model.add(Conv1D(32, 3, padding='same', activation='relu'))
+model.add(MaxPooling1D())
+model.add(Flatten())
+model.add(Dense(250, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+
+# Fitting the data onto model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=2, batch_size=128, verbose=2)
+# Getting score metrics from our model
+scores = model.evaluate(X_test, y_test, verbose=0)
+# Displays the accuracy of correct sentiment prediction over test data
+print("Accuracy: %.2f%%" % (scores[1]*100))
